4a5432c9c7da0d1d2160a5ca843b9b943fc3fa83
Deepak Singh <Deepak_S_Mehta@dell.com>
2026-03-23 12:04:26 +0800

    added captcha v3

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+ <React.StrictMode>
+    <GoogleReCaptchaProvider reCaptchaKey={"6LeyP5QsAAAAAK5ESLmnaQQLUgeqBRSFt12SEVh4"}>
+      <App />
+    </GoogleReCaptchaProvider>
   </React.StrictMode>
 );
 
